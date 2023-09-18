c96aa2ae6878eb981b258d738ab548f10bd1a135
Neil Smith <neil@neilsmith.fyi>
2023-09-17 21:29:57 -0500

    cleanup AuthorizationPolicyBuilderExtensions

--- server/src/PinIsland.Api/Extensions/Services/AuthorizationPolicyBuilderExtensions.cs
+++ server/src/PinIsland.Api/Extensions/Services/AuthorizationPolicyBuilderExtensions.cs
@@ -7,20 +7,17 @@ public static class AuthorizationPolicyBuilderExtensions
   public static void AddRequiredScopesPolicy(this AuthorizationOptions authorizationOptions, string policyName, params string[] scopes) =>
     authorizationOptions.AddPolicy(policyName, policyBuilder => policyBuilder.RequireScopes(scopes));
 
-  public static AuthorizationPolicyBuilder RequireScopes(this AuthorizationPolicyBuilder builder, params string[] scopes)
-  {
-    return builder.RequireAssertion(context =>
-     {
-       var userScopes = GetUserScopes(context);
-       return scopes.All(scope => userScopes.Contains(scope, StringComparer.CurrentCulture));
-     });
-  }
+  public static AuthorizationPolicyBuilder RequireScopes(this AuthorizationPolicyBuilder builder, params string[] scopes) =>
+    builder.RequireAssertion(context =>
+      {
+        var userScopes = GetUserScopes(context);
+        return scopes.All(scope => userScopes.Contains(scope, StringComparer.CurrentCulture));
+      });
 
-  public static List<string> GetUserScopes(this AuthorizationHandlerContext context)
-  {
-    return context?.User?
+
+  public static List<string> GetUserScopes(this AuthorizationHandlerContext context) =>
+    context?.User?
       .Claims
       .Where(c => c.Type.Equals("scope"))
       .SelectMany(c => c.Value.Split(' ')).ToList() ?? new List<string>();
-  }
 }
