6bfc3fde5898c2a629b426f4292bc2f4c529334f
Neil Smith <neil@neilsmith.fyi>
2023-09-18 15:01:07 -0500

    create permissions list

---
+++ server/src/PinIsland.Api/Domain/Permissions.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+
+namespace Linkster.Api.Domain.Authorization;
+
+public static class Permissions
+{
+  public const string CanReadRole = "role:read";
+  public const string CanReadRoles = "roles:read";
+  public const string CanCreateRole = "role:create";
+  public const string CanUpdateRole = "role:update";
+  public const string CanDeleteRole = "role:delete";
+
+  public const string CanReadUser = "user:read";
+  public const string CanReadUsers = "users:read";
+  public const string CanCreateUser = "user:create";
+  public const string CanUpdateUser = "user:update";
+  public const string CanDeleteUser = "user:delete";
+
+  public const string CanCreateRolePermission = "role-permission:create";
+  public const string CanDeleteRolePermission = "role-permission:delete";
+
+  public const string CanCreateUserRole = "user-role:create";
+  public const string CanDeleteUserRole = "user-role:delete";
+
+  public static IEnumerable<string> ToEnumerable()
+  {
+    return typeof(Permissions)
+      .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+      .Where(fieldInfo => fieldInfo.IsLiteral && !fieldInfo.IsInitOnly && fieldInfo.FieldType == typeof(string))
+      .Select(fieldInfo => (string?)fieldInfo.GetRawConstantValue() ?? "")
+      .Where(value => !string.IsNullOrEmpty(value))
+      .ToList();
+  }
+
+  public static List<string> ToList()
+  {
+    return ToEnumerable().ToList();
+  }
+}
